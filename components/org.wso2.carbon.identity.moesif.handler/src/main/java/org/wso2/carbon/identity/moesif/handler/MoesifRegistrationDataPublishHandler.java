2182fb9ed70db973b97e1a73086d783f775898f7
bhagyasakalanka <bsakalanka9@gmail.com>
2026-05-04 15:57:21 +0530

    Add Moesif user registration event handler

---
+++ components/org.wso2.carbon.identity.moesif.handler/src/main/java/org/wso2/carbon/identity/moesif/handler/MoesifRegistrationDataPublishHandler.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.moesif.handler;
+
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.identity.event.IdentityEventConstants;
+import org.wso2.carbon.identity.event.IdentityEventException;
+import org.wso2.carbon.identity.event.event.Event;
+import org.wso2.carbon.identity.event.handler.AbstractEventHandler;
+import org.wso2.carbon.identity.moesif.handler.internal.MoesifHandlerDataHolder;
+import org.wso2.carbon.identity.moesif.handler.util.MoesifHandlerUtils;
+import org.wso2.carbon.identity.organization.management.service.exception.OrganizationManagementException;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.wso2.carbon.identity.moesif.handler.constant.MoesifHandlerConstants.ACTION_NAME_USER_REGISTRATION;
+import static org.wso2.carbon.identity.moesif.handler.constant.MoesifHandlerConstants.NOT_AVAILABLE;
+import static org.wso2.carbon.identity.moesif.handler.constant.MoesifHandlerConstants.POST_ADD_USER;
+import static org.wso2.carbon.identity.moesif.handler.constant.MoesifHandlerConstants.REGISTRATION_STREAM_NAME;
+import static org.wso2.carbon.identity.moesif.handler.constant.MoesifHandlerConstants.USER_REGISTRATION_PUBLISHER_ENABLED;
+import static org.wso2.carbon.identity.moesif.handler.constant.MoesifHandlerConstants.USER_REGISTRATION_PUBLISHER_NAME;
+
+/**
+ * Event handler that publishes user registration events to Moesif via the HTTP output event adapter.
+ */
+public class MoesifRegistrationDataPublishHandler extends AbstractEventHandler {
+
+    private static final Log LOG = LogFactory.getLog(MoesifRegistrationDataPublishHandler.class);
+
+    private static final String USER_ID_CLAIM_URI = "http://wso2.org/claims/userid";
+
+    @Override
+    public String getName() {
+
+        return USER_REGISTRATION_PUBLISHER_NAME;
+    }
+
+    @Override
+    public void handleEvent(Event event) throws IdentityEventException {
+
+        if (!isEnabled(event)) {
+            return;
+        }
+
+        if (!POST_ADD_USER.equals(event.getEventName())) {
+            return;
+        }
+
+        Map<String, Object> eventProperties = event.getEventProperties();
+        String tenantDomain = (String) eventProperties.get(IdentityEventConstants.EventProperty.TENANT_DOMAIN);
+
+        @SuppressWarnings("unchecked")
+        Map<String, String> claims =
+                (Map<String, String>) eventProperties.get(IdentityEventConstants.EventProperty.USER_CLAIMS);
+        if (MapUtils.isEmpty(claims)) {
+            return;
+        }
+
+        String userId = claims.get(USER_ID_CLAIM_URI);
+        Object[] payload = MoesifHandlerUtils.buildMoesifRegistrationPayload(eventProperties, tenantDomain);
+        Optional<String> userAgent = MoesifHandlerUtils.extractUserAgent(event);
+
+        try {
+            String parentOrgId = MoesifHandlerDataHolder.getInstance().getOrganizationManager()
+                    .getParentOrganizationId(
+                            MoesifHandlerDataHolder.getInstance().getOrganizationManager()
+                                    .resolveOrganizationId(tenantDomain));
+
+            Object[] metadataArray = MoesifHandlerUtils.getMetaDataArray(parentOrgId,
+                    ACTION_NAME_USER_REGISTRATION, userId, userAgent.orElse(NOT_AVAILABLE));
+
+            org.wso2.carbon.databridge.commons.Event databridgeEvent =
+                    new org.wso2.carbon.databridge.commons.Event(
+                            REGISTRATION_STREAM_NAME, System.currentTimeMillis(),
+                            metadataArray, null, payload);
+            MoesifHandlerDataHolder.getInstance().getPublisherService().publish(databridgeEvent);
+
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Published Moesif registration event for tenant: " + tenantDomain);
+            }
+        } catch (OrganizationManagementException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isEnabled(Event event) throws IdentityEventException {
+
+        if (this.configs.getModuleProperties() != null) {
+            String handlerEnabled = this.configs.getModuleProperties()
+                    .getProperty(USER_REGISTRATION_PUBLISHER_ENABLED);
+            if (Boolean.parseBoolean(handlerEnabled)) {
+                String tenantDomain = PrivilegedCarbonContext.getThreadLocalCarbonContext().getTenantDomain();
+                return MoesifHandlerUtils.isMoesifEnabledForPrimaryTenant(tenantDomain);
+            }
+        }
+        return false;
+    }
+}
